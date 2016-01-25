29a3db186bd256836c4e7e802636b46b82143d48
engstk <eng.stk@sapo.pt>
2016-01-25 16:39:49 +0530

    Optimized Integer SQRT. for upto 3x faster operation

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -8,25 +8,34 @@
  *
  * A very rough approximation to the sqrt() function.
  */
-unsigned long int_sqrt(unsigned long x)
+inline unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long op, res, one;
+	register unsigned long tmp;
+	register unsigned long place;
+	register unsigned long root = 0;
 
-	op = x;
-	res = 0;
+	 if (x <= 1)
+ 		return x;
 
-	one = 1UL << (BITS_PER_LONG - 2);
-	while (one > op)
-		one >>= 2;
+	place = 1UL << (BITS_PER_LONG - 2);
 
-	while (one != 0) {
-		if (op >= res + one) {
-			op = op - (res + one);
-			res = res +  2 * one;
-		}
-		res /= 2;
-		one /= 4;
-	}
-	return res;
+	do{
+		place >>= 2;
+	}while(place > x);
+
+	do {
+		tmp = root + place;
+		root >>= 1;
+
+		if (x >= tmp)
+		{
+			x -= tmp;
+			root += place;		}
+		place >>= 2;
+	}while (place != 0);
+
+	return root;
+>>>>>>> bf49d5a... Optimized Integer SQRT. for upto 3x faster operation
 }
 EXPORT_SYMBOL(int_sqrt);
+
