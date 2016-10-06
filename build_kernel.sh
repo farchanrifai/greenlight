b7386aad50a1a9d3b01b93cd675581c48de0d55a
Vasishath Kaushal <vasishath@gmail.com>
2016-10-06 20:57:30 +0530

    Update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,12 @@
-CROOT=~/xenon
 DEFCONFIG=cyanogen_cancro_defconfig
-make ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-5.2-ub/bin/arm-eabi- $DEFCONFIG
-make -j32 ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-5.2-ub/bin/arm-eabi- | tee xmlog 2>&1
+make ARCH=arm CROSS_COMPILE=~/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- $DEFCONFIG
+make -j32 ARCH=arm CROSS_COMPILE=~/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 for i in `find -name *.ko`; do cp $i ~/anykernel/AnyKernel2/modules/; done
 dtbToolCM -s 2048 -d "qcom,msm-id = <" -2 -o arch/arm/boot/dt.img -p /usr/bin/ arch/arm/boot/
 cp arch/arm/boot/zImage ~/anykernel/AnyKernel2/
 cp arch/arm/boot/dt.img ~/anykernel/AnyKernel2/
 cd ~/anykernel/AnyKernel2
 DATE=$(date +"%m-%d-%y")
-rm Z*
+rm *.zip
 zip -r9 ZeurionX-$DATE.zip * -x README ZeurionX-$DATE.zip
-sudo cp Z* /var/www/html/caf/
-sudo chown -R www-data:www-data /var/www/html/caf/
+cp Z* ~/public_html/
