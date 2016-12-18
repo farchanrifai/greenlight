bf90747f9712808724702a17b2ac7b146a9a3102
Farchan Rifai <farchanrifaii@gmail.com>
2016-12-18 22:04:30 +0700

    Add Blu_active governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -427,6 +427,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_optimax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_blu_active)
 #endif
 
 
