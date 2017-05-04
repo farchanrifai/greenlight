6ad534a960073a921fd3db784d21f42752a8c15b
Farchan Rifai <farchanrifaii@gmail.com>
2017-05-04 16:42:26 +0700

    Update build script

--- build.sh
+++
@@ -1,3 +0,0 @@
-DEFCONFIG=cyanogen_cancro_defconfig
-make ARCH=arm CROSS_COMPILE=~/workspace/toolchain/bin/arm-eabi- $DEFCONFIG
-make CONFIG_NO_ERROR_ON_MISMATCH=y -j32 ARCH=arm CROSS_COMPILE=~/workspace/toolchain/bin/arm-eabi-
